0693183b3438cc82f6c1a236d2bc8e0ad18ec009
lemondreamtobe <897418436@qq.com>
2018-06-12 14:29:53 +0800

    first add

---
+++ src/page/demo.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="demo">
+        <div id="header"></div>
+        <div>
+            <div id="nav"></div>
+            <div id="content"></div>
+        </div>
+        <div id="footer"></div>
+    </div>
+</template>
