84a414918f55d0fe693815a7b88c478b437f252d
谢敏辉 <xmh19940228@163.com>
2018-06-12 17:01:01 +0800

    gm-blog-project
    添加内部技术分享网站项目

--- src/page/demo.vue
+++ src/page/demo.vue
@@ -8,3 +8,38 @@
         <div id="footer"></div>
     </div>
 </template>
+<script>
+  export default {
+    data(){
+      return{
+
+      }
+    }
+  }
+</script>
+<style type="text/scss" lang="scss" scoped>
+  #demo{
+    padding: 10px;
+    #header{
+      background: red;
+      margin: 5px 0px;
+      height: 120px;
+    }
+    #nav{
+      float: left;
+      background: gray;
+      width: 200px;
+      height: 500px;
+    }
+    #content{
+      margin-left: 210px;
+      background: burlywood;
+      height: 500px;
+    }
+    #footer{
+      margin: 5px 0px;
+      background: orangered;
+      height: 120px;
+    }
+  }
+</style>
