16c5e81c7de803913430ad22d3c25b609d0cd7df
谢敏辉 <xmh19940228@163.com>
2018-06-14 10:19:38 +0800

    M 更改公用样式mixin.scss位置，增加leftside模拟数据

--- src/server/mock/leftside.js
+++ src/server/mock/leftside.js
@@ -1,3 +1,15 @@
 export const leftUl  = {
   code: 0,
+  data: {
+    'list|3-5': [{
+      // 属性 id 是一个自增数，起始值为 1，每次增 1
+      'id|+1': 1,
+      'name|1' : ['lemon zhang','dragon zheng','liny xie'],
+      'ArticleList|1-3': [
+        {
+          'name|1':['2018-05-08' , '2018-05-09', '2018-05-10']
+        }
+      ]
+    }]
+  }
 }
